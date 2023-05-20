38dd8ef68135d2b50696bebc5bbe0926a205502d
Aylamccarthy <>
2023-05-20 10:50:02 +0000

    Add email_address method

--- test_student.py
+++ test_student.py
@@ -3,13 +3,29 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    def setUp(self):
+        print('SetUp')
+        self.student = Student('John', 'Doe')
 
-        self.assertEqual(student.full_name, 'John Doe')
+    def tearDown(self):
+        print('tearDown')
+
+
+    def test_full_name(self):
+        print('test_full_name')
+
+        self.assertEqual(self.student.full_name, 'John Doe')
+
+    def test_alert_santa(self):
+        print('test_alert_santa')
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email_address(self):
+        print('test_email_address')
+        self.assertEqual(self.student.email_address, 'john.doe@email.com')
+
 
 if __name__ == '__main__':
     unittest.main()
-
-    
-
